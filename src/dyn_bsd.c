18c53b091a87d562f39e72e999a7603b64c855a9
Ludovic Rousseau <ludovic.rousseau@free.fr>
2002-04-04 23:46:26 +0000

    DYN_GetAddress() now correctly handles errors by not using dlerror() to know if an error occured since the error may have occured in a previous call Also removed dlerror() from if tests everywhere else
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@28 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- src/dyn_bsd.c
+++ src/dyn_bsd.c
@@ -1,14 +1,14 @@
 /******************************************************************
 
 	MUSCLE SmartCard Development ( http://www.linuxnet.com )
-	    Title  : dyn_bsd.c
-	    Package: pcsc lite
-            Author : David Corcoran
-            Date   : 8/12/99
-	    License: Copyright (C) 1999 David Corcoran
-	             <corcoran@linuxnet.com>
-            Purpose: This abstracts dynamic library loading 
-                     functions and timing. 
+    Title  : dyn_bsd.c
+    Package: pcsc lite
+	Author : David Corcoran
+	Date   : 8/12/99
+	License: Copyright (C) 1999 David Corcoran <corcoran@linuxnet.com>
+	Purpose: This abstracts dynamic library loading functions and timing. 
+
+$Id$
 
 ********************************************************************/
 
@@ -24,20 +24,12 @@
 
 int DYN_LoadLibrary(void **pvLHandle, char *pcLibrary)
 {
-
-	const char *error;
-
-	/*
-	 * Zero out everything 
-	 */
-	error = NULL;
-
-	*pvLHandle = 0;
+	*pvLHandle = NULL;
 	*pvLHandle = dlopen(pcLibrary, RTLD_LAZY);
 
-	if ((error = dlerror()) != NULL)
+	if (*pvLHandle == NULL)
 	{
-		DebugLogB("DYN_LoadLibrary: dlerror() reports %s", error);
+		DebugLogB("DYN_LoadLibrary: dlerror() reports %s", dlerror());
 		return SCARD_F_UNKNOWN_ERROR;
 	}
 
@@ -46,20 +38,14 @@ int DYN_LoadLibrary(void **pvLHandle, char *pcLibrary)
 
 int DYN_CloseLibrary(void **pvLHandle)
 {
+	int ret;
 
-	const char *error;
+	ret = dlclose(*pvLHandle);
+	*pvLHandle = NULL;
 
-	/*
-	 * Zero out everything 
-	 */
-	error = NULL;
-
-	dlclose(*pvLHandle);
-	*pvLHandle = 0;
-
-	if ((error = dlerror()) != NULL)
+	if (ret)
 	{
-		DebugLogB("DYN_CloseLibrary: dlerror() reports %s", error);
+		DebugLogB("DYN_CloseLibrary: dlerror() reports %s", dlerror());
 		return SCARD_F_UNKNOWN_ERROR;
 	}
 
@@ -68,17 +54,14 @@ int DYN_CloseLibrary(void **pvLHandle)
 
 int DYN_GetAddress(void *pvLHandle, void **pvFHandle, char *pcFunction)
 {
-
 	int rv, iSize;
-	const char *error;
 	char *pcFunctionName;
 
 	/*
 	 * Zero out everything 
 	 */
 	rv = 0;
-	error = 0;
-	pcFunctionName = 0;
+	pcFunctionName = NULL;
 	iSize = 0;
 
 	iSize = strlen(pcFunction) + 2;	/* 1-NULL, 2-_ */
@@ -95,17 +78,15 @@ int DYN_GetAddress(void *pvLHandle, void **pvFHandle, char *pcFunction)
 	if (*pvFHandle == NULL)
 		*pvFHandle = dlsym(pvLHandle, pcFunction);
 
-	if ((error = dlerror()) != 0)
+	if (*pvFHandle == NULL)
 	{
-		DebugLogB("DYN_GetAddress: dlerror() reports %s", error);
+		DebugLogB("DYN_GetAddress: dlerror() reports %s", dlerror());
 		rv = SCARD_F_UNKNOWN_ERROR;
 	} else
-	{
 		rv = SCARD_S_SUCCESS;
-	}
 
 	free(pcFunctionName);
-	pcFunctionName = NULL;
 
 	return rv;
 }
+
