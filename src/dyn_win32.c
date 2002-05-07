5e652c38479dedc0bd6ea863a470b334f97905db
David Corcoran <invalid@example.com>
2002-05-07 02:02:55 +0000

    Added Win32 dynamic lib loading
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@81 0ce88b0d-b2fd-0310-8134-9614164e65ea

---
+++ src/dyn_win32.c
@@ -0,0 +1,81 @@
+/******************************************************************
+
+	MUSCLE SmartCard Development ( http://www.linuxnet.com )
+	Title  : dyn_unix.c
+	Package: pcsc lite
+	Author : David Corcoran
+	Date   : 8/12/99
+	License: Copyright (C) 1999 David Corcoran
+			<corcoran@linuxnet.com>
+	Purpose: This abstracts dynamic library loading functions and timing. 
+
+$Id$
+
+********************************************************************/
+
+#include <string.h>
+//#include <dlfcn.h>
+
+#include "../win32/win32_config.h"
+#include "windows.h"
+#include <winscard.h>
+#include "dyn_generic.h"
+#include "debuglog.h"
+
+int DYN_LoadLibrary(void **pvLHandle, char *pcLibrary)
+{
+	*pvLHandle = 0;
+	*pvLHandle = LoadLibrary(pcLibrary);
+
+	if (*pvLHandle == NULL)
+	{
+		//DebugLogB("DYN_LoadLibrary: dlerror() reports %s", dlerror());
+		return SCARD_F_UNKNOWN_ERROR;
+	}
+
+	return SCARD_S_SUCCESS;
+}
+
+int DYN_CloseLibrary(void **pvLHandle)
+{
+	int ret;
+
+	ret = FreeLibrary(*pvLHandle);
+	*pvLHandle = 0;
+
+	if (ret)
+	{
+		//DebugLogB("DYN_CloseLibrary: dlerror() reports %s", dlerror());
+		return SCARD_F_UNKNOWN_ERROR;
+	}
+
+	return SCARD_S_SUCCESS;
+}
+
+int DYN_GetAddress(void *pvLHandle, void **pvFHandle, char *pcFunction)
+{
+	int rv;
+	char *pcFunctionName;
+
+	/*
+	 * Zero out everything 
+	 */
+	rv = 0;
+	pcFunctionName = 0;
+
+	pcFunctionName = pcFunction;
+
+	*pvFHandle = 0;
+	*pvFHandle = GetProcAddress(pvLHandle, pcFunctionName);
+
+	if (*pvFHandle == NULL)
+	{
+		//DebugLogB("DYN_GetAddress: dlerror() reports %s", dlerror());
+		rv = SCARD_F_UNKNOWN_ERROR;
+	}
+	else
+		rv = SCARD_S_SUCCESS;
+
+	return rv;
+}
+
