d9d84784db625510c45ae66575cf6631d0c2e17b
Ludovic Rousseau <ludovic.rousseau@free.fr>
2003-08-11 09:55:36 +0000

    add LTPBundleFindValueWithKey() function prototype
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@265 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- src/PCSC/parser.h
+++ src/PCSC/parser.h
@@ -1,5 +1,22 @@
+/*****************************************************************
+
+  File   :   parser.h
+  Author :   Toni Andjelkovic
+  Date   :   August 11, 2003
+  Purpose:   Reads lexical config files and updates database.
+             See http://www.linuxnet.com for more information.
+  License:   Copyright (C) 2003 Toni Andjelkovic <toni@soth.at>
+             Ludovic Rousseau <ludovic.rousseau@free.fr>
+$Id$
+
+******************************************************************/
+
 #define TOKEN_MAX_KEY_SIZE   200
 #define TOKEN_MAX_VALUE_SIZE 200
 
 #define TOKEN_TYPE_KEY         1
 #define TOKEN_TYPE_STRING      2
+
+int LTPBundleFindValueWithKey(char *fileName, char *tokenKey,
+                              char *tokenValue, int tokenIndice);
+
