2022e3676502752b3afafce4a8d631358501dbbd
Ludovic Rousseau <ludovic.rousseau@free.fr>
2004-09-07 08:35:00 +0000

    prototypes of strlcpy/strlcat
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@1220 0ce88b0d-b2fd-0310-8134-9614164e65ea

---
+++ src/strlcpycat.h
@@ -0,0 +1,19 @@
+/*
+ * prototypes of strlcpy()/strlcat() imported from OpenBSD
+ *
+ * MUSCLE SmartCard Development ( http://www.linuxnet.com )
+ *
+ * Copyright (C) 2004
+ *  Ludovic Rousseau <ludovic.rousseau@free.fr>
+ *
+ * $Id$
+ */
+
+#ifndef HAVE_STRLCPY
+size_t strlcpy(char *dst, const char *src, size_t siz);
+#endif
+
+#ifndef HAVE_STRLCAT
+size_t strlcat(char *dst, const char *src, size_t siz);
+#endif
+
