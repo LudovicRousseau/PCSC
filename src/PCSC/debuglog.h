a6af72245628c10c161ceb73bd8b3fe1fd8a02f6
Ludovic Rousseau <ludovic.rousseau@free.fr>
2005-03-01 09:14:59 +0000

    add () after the function name
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@1381 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- src/PCSC/debuglog.h
+++ src/PCSC/debuglog.h
@@ -64,10 +64,12 @@ enum {
 #define __FUNCTION__ ""
 #endif
 
-#define Log0(priority) log_msg(priority, "%s:%d:%s", __FILE__, __LINE__, __FUNCTION__)
-#define Log1(priority, fmt) log_msg(priority, "%s:%d:%s " fmt, __FILE__, __LINE__, __FUNCTION__)
-#define Log2(priority, fmt, data) log_msg(priority, "%s:%d:%s " fmt, __FILE__, __LINE__, __FUNCTION__, data)
-#define Log3(priority, fmt, data1, data2) log_msg(priority, "%s:%d:%s " fmt, __FILE__, __LINE__, __FUNCTION__, data1, data2)
+#define Log0(priority) log_msg(priority, "%s:%d:%s()", __FILE__, __LINE__, __FUNCTION__)
+#define Log1(priority, fmt) log_msg(priority, "%s:%d:%s() " fmt, __FILE__, __LINE__, __FUNCTION__)
+#define Log2(priority, fmt, data) log_msg(priority, "%s:%d:%s() " fmt, __FILE__, __LINE__, __FUNCTION__, data)
+#define Log3(priority, fmt, data1, data2) log_msg(priority, "%s:%d:%s() " fmt, __FILE__, __LINE__, __FUNCTION__, data1, data2)
+#define Log3(priority, fmt, data1, data2) log_msg(priority, "%s:%d:%s() " fmt, __FILE__, __LINE__, __FUNCTION__, data1, data2)
+#define Log4(priority, fmt, data1, data2, data3) log_msg(priority, "%s:%d:%s() " fmt, __FILE__, __LINE__, __FUNCTION__, data1, data2, data3)
 #define LogXxd(priority, msg, buffer, size) log_xxd(priority, msg, buffer, size)
 
 #define DebugLogA(a) Log1(PCSC_LOG_INFO, a)
