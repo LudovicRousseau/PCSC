db5acea30d0d7d3c5464d733783da41357b78a08
Ludovic Rousseau <ludovic.rousseau@free.fr>
2006-01-24 10:38:09 +0000

    reformat comment
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@1821 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- win32/PCSC.h
+++ win32/PCSC.h
@@ -1,10 +1,11 @@
 
-// The following ifdef block is the standard way of creating macros which make exporting 
-// from a DLL simpler. All files within this DLL are compiled with the PCSC_EXPORTS
-// symbol defined on the command line. this symbol should not be defined on any project
-// that uses this DLL. This way any other project whose source files include this file see 
-// PCSC_API functions as being imported from a DLL, wheras this DLL sees symbols
-// defined with this macro as being exported.
+// The following ifdef block is the standard way of creating macros which make
+// exporting from a DLL simpler. All files within this DLL are compiled with
+// the PCSC_EXPORTS symbol defined on the command line. this symbol should not
+// be defined on any project that uses this DLL. This way any other project
+// whose source files include this file see PCSC_API functions as being
+// imported from a DLL, wheras this DLL sees symbols defined with this macro as
+// being exported.
 #ifdef PCSC_EXPORTS
 #define PCSC_API __declspec(dllexport)
 #else
