e0766f0f41c2f84a16b3b5e2ac02ebceefb143b0
Ludovic Rousseau <ludovic.rousseau@free.fr>
2008-02-01 09:36:17 +0000

    add TAG_IFD_POLLING_THREAD
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@2779 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- src/PCSC/ifdhandler.h
+++ src/PCSC/ifdhandler.h
@@ -97,6 +97,7 @@ extern "C"
 #define TAG_IFD_THREAD_SAFE             0x0FAD
 #define TAG_IFD_SLOTS_NUMBER            0x0FAE
 #define TAG_IFD_SIMULTANEOUS_ACCESS     0x0FAF
+#define TAG_IFD_POLLING_THREAD          0x0FB0
 
 	/*
 	 * End of tag list
