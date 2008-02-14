107cbd8c670c642b17ad0f487c3c5b3c9265fd52
Ludovic Rousseau <ludovic.rousseau@free.fr>
2008-02-14 10:49:57 +0000

    move types unused by pcsc-lite at the end
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@2814 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- src/PCSC/wintypes.h
+++ src/PCSC/wintypes.h
@@ -32,14 +32,11 @@ extern "C"
 #ifndef __COREFOUNDATION_CFPLUGINCOM__
 	typedef unsigned long ULONG;
 	typedef void *LPVOID;
-	typedef short BOOL;
 #endif
 
-	typedef unsigned long *PULONG;
 	typedef const void *LPCVOID;
 	typedef unsigned long DWORD;
 	typedef unsigned long *PDWORD;
-	typedef unsigned short WORD;
 	typedef long LONG;
 	typedef const char *LPCSTR;
 	typedef const BYTE *LPCBYTE;
@@ -51,6 +48,11 @@ extern "C"
 	 * applications. So just declare and use them. */
 	typedef LPSTR LPTSTR;
 	typedef LPCSTR LPCTSTR;
+
+	/* types unused by pcsc-lite */
+	typedef short BOOL;
+	typedef unsigned short WORD;
+	typedef unsigned long *PULONG;
 
 #else
 #include <windows.h>
