9acfe47a2a296996612c5a28478ef77779106878
Ludovic Rousseau <ludovic.rousseau@free.fr>
2008-04-21 13:22:44 +0000

    doxygen: convert C comments in doxygen comments
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@2892 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- src/PCSC/ifdhandler.h
+++ src/PCSC/ifdhandler.h
@@ -29,49 +29,49 @@ extern "C"
 
 	typedef struct _DEVICE_CAPABILITIES
 	{
-		LPSTR Vendor_Name;		/* Tag 0x0100 */
-		LPSTR IFD_Type;			/* Tag 0x0101 */
-		DWORD IFD_Version;		/* Tag 0x0102 */
-		LPSTR IFD_Serial;		/* Tag 0x0103 */
-		DWORD IFD_Channel_ID;	/* Tag 0x0110 */
+		LPSTR Vendor_Name;		/**< Tag 0x0100 */
+		LPSTR IFD_Type;			/**< Tag 0x0101 */
+		DWORD IFD_Version;		/**< Tag 0x0102 */
+		LPSTR IFD_Serial;		/**< Tag 0x0103 */
+		DWORD IFD_Channel_ID;	/**< Tag 0x0110 */
 
-		DWORD Asynch_Supported;	/* Tag 0x0120 */
-		DWORD Default_Clock;	/* Tag 0x0121 */
-		DWORD Max_Clock;		/* Tag 0x0122 */
-		DWORD Default_Data_Rate;	/* Tag 0x0123 */
-		DWORD Max_Data_Rate;	/* Tag 0x0124 */
-		DWORD Max_IFSD;			/* Tag 0x0125 */
-		DWORD Synch_Supported;	/* Tag 0x0126 */
-		DWORD Power_Mgmt;		/* Tag 0x0131 */
-		DWORD Card_Auth_Devices;	/* Tag 0x0140 */
-		DWORD User_Auth_Device;	/* Tag 0x0142 */
-		DWORD Mechanics_Supported;	/* Tag 0x0150 */
-		DWORD Vendor_Features;	/* Tag 0x0180 - 0x01F0 User Defined. */
+		DWORD Asynch_Supported;	/**< Tag 0x0120 */
+		DWORD Default_Clock;	/**< Tag 0x0121 */
+		DWORD Max_Clock;		/**< Tag 0x0122 */
+		DWORD Default_Data_Rate;	/**< Tag 0x0123 */
+		DWORD Max_Data_Rate;	/**< Tag 0x0124 */
+		DWORD Max_IFSD;			/**< Tag 0x0125 */
+		DWORD Synch_Supported;	/**< Tag 0x0126 */
+		DWORD Power_Mgmt;		/**< Tag 0x0131 */
+		DWORD Card_Auth_Devices;	/**< Tag 0x0140 */
+		DWORD User_Auth_Device;	/**< Tag 0x0142 */
+		DWORD Mechanics_Supported;	/**< Tag 0x0150 */
+		DWORD Vendor_Features;	/**< Tag 0x0180 - 0x01F0 User Defined. */
 	}
 	DEVICE_CAPABILITIES, *PDEVICE_CAPABILITIES;
 
 	typedef struct _ICC_STATE
 	{
-		UCHAR ICC_Presence;		/* Tag 0x0300 */
-		UCHAR ICC_Interface_Status;	/* Tag 0x0301 */
-		UCHAR ATR[MAX_ATR_SIZE];	/* Tag 0x0303 */
-		UCHAR ICC_Type;			/* Tag 0x0304 */
+		UCHAR ICC_Presence;		/**< Tag 0x0300 */
+		UCHAR ICC_Interface_Status;	/**< Tag 0x0301 */
+		UCHAR ATR[MAX_ATR_SIZE];	/**< Tag 0x0303 */
+		UCHAR ICC_Type;			/**< Tag 0x0304 */
 	}
 	ICC_STATE, *PICC_STATE;
 
 	typedef struct _PROTOCOL_OPTIONS
 	{
-		DWORD Protocol_Type;	/* Tag 0x0201 */
-		DWORD Current_Clock;	/* Tag 0x0202 */
-		DWORD Current_F;		/* Tag 0x0203 */
-		DWORD Current_D;		/* Tag 0x0204 */
-		DWORD Current_N;		/* Tag 0x0205 */
-		DWORD Current_W;		/* Tag 0x0206 */
-		DWORD Current_IFSC;		/* Tag 0x0207 */
-		DWORD Current_IFSD;		/* Tag 0x0208 */
-		DWORD Current_BWT;		/* Tag 0x0209 */
-		DWORD Current_CWT;		/* Tag 0x020A */
-		DWORD Current_EBC;		/* Tag 0x020B */
+		DWORD Protocol_Type;	/**< Tag 0x0201 */
+		DWORD Current_Clock;	/**< Tag 0x0202 */
+		DWORD Current_F;		/**< Tag 0x0203 */
+		DWORD Current_D;		/**< Tag 0x0204 */
+		DWORD Current_N;		/**< Tag 0x0205 */
+		DWORD Current_W;		/**< Tag 0x0206 */
+		DWORD Current_IFSC;		/**< Tag 0x0207 */
+		DWORD Current_IFSD;		/**< Tag 0x0208 */
+		DWORD Current_BWT;		/**< Tag 0x0209 */
+		DWORD Current_CWT;		/**< Tag 0x020A */
+		DWORD Current_EBC;		/**< Tag 0x020B */
 	}
 	PROTOCOL_OPTIONS, *PPROTOCOL_OPTIONS;
 
