4d6f12ff9b378c7b29bee00b2fcdd24bb478bb9b
Ludovic Rousseau <ludovic.rousseau@free.fr>
2009-01-05 10:35:07 +0000

    script to call splint
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@3264 0ce88b0d-b2fd-0310-8134-9614164e65ea

---
+++ splint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+# do not use debug.c and debuglog.c, they #include <syslog.h>
+# and splint does not like this include
+# do not use configfile.c since it is a lex file from configfile.l
+if [ $# -lt 1 ]
+then
+	files=$(ls -1 src/*.c | grep -v scf | grep -v debug | grep -v configfile)
+else
+	files="$@"
+fi
+inc="-I. -Isrc -Isrc/PCSC -I/usr/include/hal -I/usr/include/dbus-1.0 -I/usr/lib/dbus-1.0/include "
+opt="-warnposix -unrecog -type -predboolint -likelybool"
+
+splint $inc $opt $files
