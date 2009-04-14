df31bc770fe15732826e9de4904f4d5cbe3f5baa
Ludovic Rousseau <ludovic.rousseau@free.fr>
2009-04-14 14:01:08 +0000

    DeprecationWarning: raising a string exception is deprecated
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@3448 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- UnitaryTests/SCardCancel.py
+++ UnitaryTests/SCardCancel.py
@@ -31,11 +31,11 @@ def cancel():
 
 hresult, hcontext = SCardEstablishContext(SCARD_SCOPE_USER)
 if hresult!=0:
-	raise  'Failed to establish context: ' + SCardGetErrorMessage(hresult)
+	raise Exception('Failed to establish context: ' + SCardGetErrorMessage(hresult))
 
 hresult, readers = SCardListReaders(hcontext, [])
 if hresult!=0:
-	raise error, 'Failed to list readers: ' + SCardGetErrorMessage(hresult)
+	raise Exception('Failed to list readers: ' + SCardGetErrorMessage(hresult))
 print 'PC/SC Readers:', readers
 
 readerstates = {}
@@ -43,7 +43,7 @@ for reader in readers:
     readerstates[reader] = (reader, SCARD_STATE_UNAWARE)
 hresult, newstates = SCardGetStatusChange(hcontext, 0, readerstates.values())
 if hresult!=SCARD_S_SUCCESS:
-    raise  'Failed to SCardGetStatusChange: ' + SCardGetErrorMessage(hresult)
+    raise Exception('Failed to SCardGetStatusChange: ' + SCardGetErrorMessage(hresult))
 print newstates
 for state in newstates:
     readername, eventstate, atr = state
@@ -54,9 +54,9 @@ t.start()
 
 hresult, newstates = SCardGetStatusChange(hcontext, 10000, readerstates.values())
 if hresult!=SCARD_S_SUCCESS and hresult!=SCARD_E_TIMEOUT:
-    raise  'Failed to SCardGetStatusChange: ' + SCardGetErrorMessage(hresult)
+    raise Exception('Failed to SCardGetStatusChange: ' + SCardGetErrorMessage(hresult))
 
 hresult = SCardReleaseContext(hcontext)
 if hresult!=SCARD_S_SUCCESS:
-	raise  'Failed to release context: ' + SCardGetErrorMessage(hresult)
+	raise Exception('Failed to release context: ' + SCardGetErrorMessage(hresult))
 
