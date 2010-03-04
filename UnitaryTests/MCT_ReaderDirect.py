eabd3a89476fa008591fdfb424f2ac5455716c85
Ludovic Rousseau <ludovic.rousseau@free.fr>
2010-03-04 13:48:23 +0000

    make pylint happier
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@4792 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- UnitaryTests/MCT_ReaderDirect.py
+++ UnitaryTests/MCT_ReaderDirect.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python
 
+"""
 #   MCT_ReaderDirect.py : Unitary test for Multifunctional Card Terminal
-#   Copyright (C) 2009  Ludovic Rousseau
-#
+#   Copyright (C) 2009-2010  Ludovic Rousseau
+"""
+
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation; either version 2 of the License, or
@@ -18,7 +20,8 @@
 #   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 from smartcard.pcsc.PCSCReader import readers
-from smartcard.pcsc.PCSCPart10 import *
+from smartcard.pcsc.PCSCPart10 import (SCARD_SHARE_DIRECT,
+    SCARD_LEAVE_CARD, FEATURE_MCT_READER_DIRECT, getFeatureRequest, hasFeature)
 from smartcard.util import toHexString
 
 
@@ -56,22 +59,28 @@ def parse_select(bytes):
     print "SW:", toHexString(bytes[23:25])
 
 
-cardConnection = readers()[0].createConnection()
-cardConnection.connect(mode=SCARD_SHARE_DIRECT, disposition=SCARD_LEAVE_CARD)
+def main():
+    """ main """
+    card_connection = readers()[0].createConnection()
+    card_connection.connect(mode=SCARD_SHARE_DIRECT,
+        disposition=SCARD_LEAVE_CARD)
 
-featureList = getFeatureRequest(cardConnection)
-#print getPinProperties(cardConnection)
+    feature_list = getFeatureRequest(card_connection)
+    #print getPinProperties(card_connection)
 
-mct_readerDirect = hasFeature(featureList, FEATURE_MCT_READER_DIRECT)
-if mct_readerDirect is None:
-    raise Exception("The reader does not support MCT_READER_DIRECT")
+    mct_reader_direct = hasFeature(feature_list, FEATURE_MCT_READER_DIRECT)
+    if mct_reader_direct is None:
+        raise Exception("The reader does not support MCT_READER_DIRECT")
 
-secoder_info = [0x20, 0x70, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00]
-res = cardConnection.control(mct_readerDirect, secoder_info)
-parse_info(res)
+    secoder_info = [0x20, 0x70, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00]
+    res = card_connection.control(mct_reader_direct, secoder_info)
+    parse_info(res)
 
-secoder_select = [0x20, 0x71, 0x00, 0x00, 0x00, 0x00, 0x14, 0x00, 0x80,
-    0x05, 0x31, 0x2E, 0x31, 0x2E, 0x30, 0x84, 0x02, 0x64, 0x65,
-    0x90, 0x01, 0x01, 0x85, 0x03, ord('g'), ord('k'), ord('p'), 0x00, 0x00]
-res = cardConnection.control(mct_readerDirect, secoder_select)
-parse_select(res)
+    secoder_select = [0x20, 0x71, 0x00, 0x00, 0x00, 0x00, 0x14, 0x00, 0x80,
+        0x05, 0x31, 0x2E, 0x31, 0x2E, 0x30, 0x84, 0x02, 0x64, 0x65,
+        0x90, 0x01, 0x01, 0x85, 0x03, ord('g'), ord('k'), ord('p'), 0x00, 0x00]
+    res = card_connection.control(mct_reader_direct, secoder_select)
+    parse_select(res)
+
+if __name__ == "__main__":
+    main()
