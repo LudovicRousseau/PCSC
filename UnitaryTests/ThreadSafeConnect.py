aeff9db6150f7ca3d61c8577992f95fb49ad039d
Ludovic Rousseau <ludovic.rousseau@free.fr>
2010-03-09 14:57:36 +0000

    add more logging
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@4803 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- UnitaryTests/ThreadSafeConnect.py
+++ UnitaryTests/ThreadSafeConnect.py
@@ -31,10 +31,13 @@ MAX_THREADS = 10
 MAX_ITER = 10
 
 
-def stress():
+def stress(*args):
     """
     stress method
     """
+    thread = args[0]
+    print "Starting thread:", thread
+
     hresult, hcontext = SCardEstablishContext(SCARD_SCOPE_USER)
     if hresult != SCARD_S_SUCCESS:
         raise Exception('Failed to establish context: '
@@ -45,8 +48,6 @@ def stress():
         raise Exception('Failed to list readers: '
             + SCardGetErrorMessage(hresult))
 
-    print "Using reader:", readers[0]
-
     for j in range(0, MAX_ITER):
         # Connect in SCARD_SHARE_SHARED mode
         hresult, hcard, dwActiveProtocol = SCardConnect(hcontext, readers[0],
@@ -55,7 +56,8 @@ def stress():
             raise Exception('Failed to SCardConnect: '
                 + SCardGetErrorMessage(hresult))
 
-        print j,
+        log = "%d:%d" % (thread, j)
+        print log,
 
         hresult = SCardDisconnect(hcard, SCARD_LEAVE_CARD)
         if hresult != SCARD_S_SUCCESS:
@@ -65,10 +67,11 @@ def stress():
     print
 
     hresult = SCardReleaseContext(hcontext)
-    #print "SCardReleaseContext()", SCardGetErrorMessage(hresult)
     if hresult != SCARD_S_SUCCESS:
         raise Exception('Failed to release context: '
             + SCardGetErrorMessage(hresult))
+
+    print "Exiting thread:", thread
 
 def main():
     """
@@ -77,12 +80,16 @@ def main():
     threads = list()
 
     for i in range(0, MAX_THREADS):
-        thread = threading.Thread(target=stress)
+        thread = threading.Thread(target=stress, args=[i])
         threads.append(thread)
         thread.start()
 
     for thread in threads:
+        print "joining:", thread.getName(),
         thread.join()
+        print "joined:", thread.getName()
+
+    print "Exiting main"
 
 if __name__ == "__main__":
     main()
