5cd9bc629e3b76518ca90079ea2e4ff1cc37bee1
Ludovic Rousseau <ludovic.rousseau@free.fr>
2010-05-20 15:08:45 +0000

    get SCARD_ATTR_VENDOR_IFD_SERIAL_NO PC/SC attribute
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@4957 0ce88b0d-b2fd-0310-8134-9614164e65ea

---
+++ UnitaryTests/SCardGetAttrib.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+
+"""
+#   SCardGetAttrib.py: get SCARD_ATTR_VENDOR_IFD_SERIAL_NO PC/SC attribute
+#   Copyright (C) 2010  Ludovic Rousseau
+"""
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# SCARD_ATTR_VENDOR_IFD_SERIAL_NO support has been added in revision 4956
+
+from smartcard.scard import *
+import sys
+
+hresult, hcontext = SCardEstablishContext(SCARD_SCOPE_USER)
+if hresult != SCARD_S_SUCCESS:
+    raise Exception('Failed to establish context: ' + SCardGetErrorMessage(hresult))
+
+hresult, readers = SCardListReaders(hcontext, [])
+if hresult != SCARD_S_SUCCESS:
+    raise Exception('Failed to list readers: ' + SCardGetErrorMessage(hresult))
+print 'PC/SC Readers:', readers
+
+for reader in readers:
+    hresult, hcard, dwActiveProtocol = SCardConnect(hcontext, reader,
+        SCARD_SHARE_DIRECT, SCARD_PROTOCOL_ANY)
+    if hresult != SCARD_S_SUCCESS:
+        raise Exception('Failed to SCardConnect: ' + SCardGetErrorMessage(hresult))
+
+    hresult, attrib = SCardGetAttrib(hcard, SCARD_ATTR_VENDOR_IFD_SERIAL_NO)
+    print reader, ":",
+    print "".join([chr(x) for x in attrib])
+    print SCardGetErrorMessage(hresult)
+
+    hresult = SCardDisconnect(hcard, SCARD_LEAVE_CARD)
+    if hresult != SCARD_S_SUCCESS:
+        raise Exception('Failed to SCardDisconnect: ' + SCardGetErrorMessage(hresult))
+
+hresult = SCardReleaseContext(hcontext)
+if hresult != SCARD_S_SUCCESS:
+    raise Exception('Failed to release context: ' + SCardGetErrorMessage(hresult))
