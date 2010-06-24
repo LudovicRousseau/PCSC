50958da001a754cf504970b3a9c47fc55cbaa455
Ludovic Rousseau <ludovic.rousseau@free.fr>
2010-06-24 09:23:58 +0000

    send an apdu in loop
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@5036 0ce88b0d-b2fd-0310-8134-9614164e65ea

---
+++ UnitaryTests/stress_apdu.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+
+"""
+    stress_apdu.py: send an apdu in loop
+    Copyright (C) 2010  Ludovic Rousseau
+"""
+
+#   this program is free software; you can redistribute it and/or modify
+#   it under the terms of the gnu general public license as published by
+#   the free software foundation; either version 2 of the license, or
+#   (at your option) any later version.
+#
+#   this program is distributed in the hope that it will be useful,
+#   but without any warranty; without even the implied warranty of
+#   merchantability or fitness for a particular purpose.  see the
+#   gnu general public license for more details.
+#
+#   you should have received a copy of the gnu general public license along
+#   with this program; if not, write to the free software foundation, inc.,
+#   51 franklin street, fifth floor, boston, ma 02110-1301 usa.
+
+from smartcard.System import readers
+
+
+def stress(reader):
+    # define the APDUs used in this script
+    SELECT = [0x00, 0xA4, 0x04, 0x00, 0x0A, 0xA0, 0x00, 0x00, 0x00, 0x62,
+        0x03, 0x01, 0x0C, 0x06, 0x01]
+    COMMAND = [0x00, 0x00, 0x00, 0x00]
+
+    connection = reader.createConnection()
+    connection.connect()
+
+    data, sw1, sw2 = connection.transmit(SELECT)
+    print data
+    print "Select Applet: %02X %02X" % (sw1, sw2)
+
+    i = 0
+    while True:
+        data, sw1, sw2 = connection.transmit(COMMAND)
+        print data
+        print "%d Command: %02X %02X" % (i, sw1, sw2)
+        i += 1
+
+if __name__ == "__main__":
+    import sys
+
+    # get all the available readers
+    readers = readers()
+    print "Available readers:", readers
+
+    try:
+        i = int(sys.argv[1])
+    except IndexError:
+        i = 0
+
+    reader = readers[i]
+    print "Using:", reader
+
+    stress(reader)
