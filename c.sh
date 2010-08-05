b83188394c2b7f39883985b0b2c401c1aa19df4f
Ludovic Rousseau <ludovic.rousseau@free.fr>
2010-08-05 07:28:19 +0000

    No need to use -D_REENTRANT. It was needed for LinuxThreads but we do not use LinuxThreads since a long time.
    See Debian bug #475101 "Remove obsolete -D_REENTRANT requirement"
    
    
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@5111 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- c.sh
+++ c.sh
@@ -4,8 +4,6 @@ set -x
 
 #CFLAGS="$CFLAGS -Wall -g -O2 -Wextra -pipe -funsigned-char -fstrict-aliasing -Wchar-subscripts -Wundef -Wshadow -Wcast-align -Wwrite-strings -Wunused -Wno-unused-value -Wuninitialized -Wpointer-arith -Wredundant-decls -Wmissing-prototypes -Wstrict-prototypes -Wold-style-definition -Wmissing-declarations -Wbad-function-cast -Winline -Wnested-externs -Wformat-security -Wswitch-enum -Winit-self -Wmissing-include-dirs -Wno-unused-parameter -Wno-sign-compare"
 
-CFLAGS="$CFLAGS -D_REENTRANT"
-
 ./configure \
         --prefix=/usr \
         --enable-maintainer-mode \
