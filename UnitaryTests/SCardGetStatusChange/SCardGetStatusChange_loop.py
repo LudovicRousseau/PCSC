8e3803659c5143810676c45fa76a35c4e1423c1c
Ludovic Rousseau <ludovic.rousseau@free.fr>
2010-10-05 14:06:43 +0000

    Call SCardGetStatusChange() in a endless loop
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@5280 0ce88b0d-b2fd-0310-8134-9614164e65ea

---
+++ UnitaryTests/SCardGetStatusChange/SCardGetStatusChange_loop.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+#   SCardGetStatusChange.py : Unit tests for unlock a SCardGetStatusChange()
+#   Copyright (C) 2010  Ludovic Rousseau
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+from smartcard.System import readers
+from smartcard.scard import *
+
+import sys
+
+hresult, hcontext = SCardEstablishContext(SCARD_SCOPE_USER)
+hresult, readers = SCardListReaders(hcontext, [])
+
+# initialise the states of the readers
+readerstates = {}
+for reader in readers:
+    readerstates[reader] = (reader, SCARD_STATE_UNAWARE)
+print "values", readerstates.values()
+(hresult, states) = SCardGetStatusChange(hcontext, 0, readerstates.values())
+print SCardGetErrorMessage(hresult)
+print states
+
+for state in states:
+    readername, eventstate, atr = state
+    print "readername:", readername
+    print "eventstate:", hex(eventstate)
+    print "atr:", atr
+    readerstates[readername] = (readername, eventstate)
+print "values", readerstates.values()
+
+while 1:
+    # timeout is 1000 ms
+    (hresult, states) = SCardGetStatusChange(hcontext, 1000, readerstates.values())
+    print SCardGetErrorMessage(hresult)
+    print states
+
+    for state in states:
+        print state
+        readername, eventstate, atr = state
+        print "readername:", readername
+        print "eventstate:", hex(eventstate)
+        print "atr:", atr
+        readerstates[readername] = (readername, eventstate)
+    print "values", readerstates.values()
+
+hresult = SCardReleaseContext(hcontext)
+print SCardGetErrorMessage(hresult)
