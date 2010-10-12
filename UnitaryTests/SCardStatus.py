0285caa53ce86a6f7a6502d77241dc76e7f84697
Ludovic Rousseau <ludovic.rousseau@free.fr>
2010-10-12 11:35:47 +0000

    Unit test for SCardStatus()
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@5302 0ce88b0d-b2fd-0310-8134-9614164e65ea

---
+++ UnitaryTests/SCardStatus.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+#   SCardStatus.py : Unit test for SCardStatus()
+#   Copyright (C) 2010  Ludovic Rousseau
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+from smartcard.scard import *
+from smartcard.pcsc.PCSCExceptions import *
+
+
+hresult, hcontext = SCardEstablishContext(SCARD_SCOPE_USER)
+if hresult != SCARD_S_SUCCESS:
+    raise EstablishContextException(hresult)
+
+hresult, readers = SCardListReaders(hcontext, [])
+if hresult != SCARD_S_SUCCESS:
+    raise ListReadersException(hresult)
+print 'PC/SC Readers:', readers
+
+reader = readers[0]
+print "Using reader:", reader
+
+# Connect in SCARD_SHARE_SHARED mode
+hresult, hcard, dwActiveProtocol = SCardConnect(hcontext, reader,
+    SCARD_SHARE_SHARED, SCARD_PROTOCOL_ANY)
+if hresult != SCARD_S_SUCCESS:
+    raise BaseSCardException(hresult)
+
+hresult, reader, state, protocol, atr = SCardStatus(hcard)
+if hresult != SCARD_S_SUCCESS:
+    raise BaseSCardException(hresult)
+print SCardGetErrorMessage(hresult)
+
+print "reader:", reader
+print "state:", state
+print "protocol:", protocol
+print "atr:", atr
+
+hresult = SCardReleaseContext(hcontext)
+print SCardGetErrorMessage(hresult)
