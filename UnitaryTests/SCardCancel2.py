4453db912ad5010294f9e2d1de876a272c82ee7e
Ludovic Rousseau <ludovic.rousseau@free.fr>
2010-10-22 12:53:07 +0000

    SCardCancel() should do nothing if no cancellable call is ongoing bug fixed in revision 5344
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@5345 0ce88b0d-b2fd-0310-8134-9614164e65ea

---
+++ UnitaryTests/SCardCancel2.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+
+#   SCardCancel2.py : Unitary test for SCardCancel()
+#   Copyright (C) 2010  Jan Rochat
+#   https://bugs.launchpad.net/ubuntu/+source/pcsc-lite/+bug/647545/comments/6
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# SCardCancel() should do nothing if no cancellable call is ongoing
+# bug fixed in revision 5344
+
+from smartcard.scard import *
+
+try:
+    hresult, hcontext = SCardEstablishContext(SCARD_SCOPE_USER)
+    if hresult != SCARD_S_SUCCESS:
+        raise Exception('Failed to establish context : ' +
+                        SCardGetErrorMessage(hresult))
+    print 'Context established!'
+
+    try:
+        hresult = SCardCancel(hcontext)
+        if hresult != SCARD_S_SUCCESS:
+            raise Exception('Failed to cancel context : ' +
+                            SCardGetErrorMessage(hresult))
+        print 'context canceled'
+
+    finally:
+        hresult = SCardReleaseContext(hcontext)
+        if hresult != SCARD_S_SUCCESS:
+            raise Exception('Failed to release context: ' +
+                            SCardGetErrorMessage(hresult))
+        print 'Released context.'
+
+except Exception, message:
+    print "Exception:", message
