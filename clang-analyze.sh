3ca2e1b547e384ad4c7b7032076a4466f7f0286f
Ludovic Rousseau <ludovic.rousseau@free.fr>
2011-01-18 15:44:44 +0000

    script to call clang --analyze
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@5520 0ce88b0d-b2fd-0310-8134-9614164e65ea

---
+++ clang-analyze.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+# do not use configfile.c since it is a lex file from configfile.l
+if [ $# -lt 1 ]
+then
+	files=$(ls -1 src/*.c | grep -v configfile)
+else
+	files="$@"
+fi
+inc="-I. -Isrc -Isrc/PCSC -I/usr/include/hal -I/usr/include/dbus-1.0 -I/usr/lib/dbus-1.0/include "
+opt="--analyze "
+
+clang $inc $opt $files
