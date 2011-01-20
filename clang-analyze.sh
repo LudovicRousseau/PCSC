8ed1f3c88dfb95d115aef5ab8237bf08fea4d463
Ludovic Rousseau <ludovic.rousseau@free.fr>
2011-01-20 09:27:59 +0000

    Do not analyze tokenparser.c since it is generated by flex
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@5533 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- clang-analyze.sh
+++ clang-analyze.sh
@@ -5,7 +5,7 @@ set -e
 # do not use configfile.c since it is a lex file from configfile.l
 if [ $# -lt 1 ]
 then
-	files=$(ls -1 src/*.c | grep -v configfile)
+	files=$(ls -1 src/*.c | grep -v configfile | grep -v tokenparser)
 else
 	files="$@"
 fi
