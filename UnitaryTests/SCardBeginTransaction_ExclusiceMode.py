024102ab2d0c90e678f5ecb2bf8b56504eccbaa4
Ludovic Rousseau <ludovic.rousseau@free.fr>
2011-02-01 16:03:32 +0000

    Unitary test for Alioth bug [#312960] SCardDisconnect when other context has transaction
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@5575 0ce88b0d-b2fd-0310-8134-9614164e65ea

---
+++ UnitaryTests/SCardBeginTransaction_ExclusiceMode.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python
+
+#   SCardBeginTransaction_ExclusiceMode.py : Unitary test for SCardDisconnect()
+#   Copyright (C) 2011  Ludovic Rousseau
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Alioth bug [#312960] SCardDisconnect when other context has transaction
+# fixed in revisions 5572 and 5574
+
+from smartcard.scard import *
+from smartcard.pcsc.PCSCExceptions import *
+
+hresult, hcontext1 = SCardEstablishContext(SCARD_SCOPE_USER)
+if hresult != SCARD_S_SUCCESS:
+    raise EstablishContextException(hresult)
+
+hresult, readers = SCardListReaders(hcontext1, [])
+if hresult != SCARD_S_SUCCESS:
+    raise ListReadersException(hresult)
+print 'PC/SC Readers:', readers
+reader = readers[0]
+print "Using reader:", reader
+
+hresult, hcontext2 = SCardEstablishContext(SCARD_SCOPE_USER)
+if hresult != SCARD_S_SUCCESS:
+    raise EstablishContextException(hresult)
+
+hresult, hcard1, dwActiveProtocol = SCardConnect(hcontext1, reader, SCARD_SHARE_SHARED, SCARD_PROTOCOL_ANY)
+if hresult != SCARD_S_SUCCESS:
+    raise BaseSCardException(hresult)
+
+hresult, hcard2, dwActiveProtocol = SCardConnect(hcontext1, reader, SCARD_SHARE_SHARED, SCARD_PROTOCOL_ANY)
+if hresult != SCARD_S_SUCCESS:
+    raise BaseSCardException(hresult)
+
+hresult = SCardBeginTransaction(hcard1)
+if hresult != SCARD_S_SUCCESS:
+    raise BaseSCardException(hresult)
+
+# Disconnect hcard2 before the end of transaction with hcard1
+hresult = SCardDisconnect(hcard2, SCARD_LEAVE_CARD)
+if hresult != SCARD_S_SUCCESS:
+    raise BaseSCardException(hresult)
+
+hresult = SCardEndTransaction(hcard1, SCARD_LEAVE_CARD)
+if hresult != SCARD_S_SUCCESS:
+    raise BaseSCardException(hresult)
+
+hresult = SCardDisconnect(hcard1, SCARD_LEAVE_CARD)
+if hresult != SCARD_S_SUCCESS:
+    raise BaseSCardException(hresult)
+
+# Connect should not return an error since hcard1 and hcard2 are now
+# disconnected
+hresult, hcard1, dwActiveProtocol = SCardConnect(hcontext1, reader, SCARD_SHARE_EXCLUSIVE, SCARD_PROTOCOL_ANY)
+if hresult != SCARD_S_SUCCESS:
+    raise BaseSCardException(hresult)
+
+hresult = SCardDisconnect(hcard1, SCARD_LEAVE_CARD)
+if hresult != SCARD_S_SUCCESS:
+    raise BaseSCardException(hresult)
+
+hresult = SCardReleaseContext(hcontext1)
+if hresult != SCARD_S_SUCCESS:
+    raise ReleaseContextException(hresult)
