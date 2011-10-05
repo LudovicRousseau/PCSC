124f6e01ee3e4075c320489b99d8ba008e3d8da4
Ludovic Rousseau <ludovic.rousseau@free.fr>
2011-10-05 13:19:37 +0000

    Move from GPL v2+ to GPL v3+
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@6002 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- UnitaryTests/stress_get_firmware.py
+++ UnitaryTests/stress_get_firmware.py
@@ -7,7 +7,7 @@
 
 #   this program is free software; you can redistribute it and/or modify
 #   it under the terms of the gnu general public license as published by
-#   the free software foundation; either version 2 of the license, or
+#   the free software foundation; either version 3 of the license, or
 #   (at your option) any later version.
 #
 #   this program is distributed in the hope that it will be useful,
@@ -16,8 +16,7 @@
 #   gnu general public license for more details.
 #
 #   you should have received a copy of the gnu general public license along
-#   with this program; if not, write to the free software foundation, inc.,
-#   51 franklin street, fifth floor, boston, ma 02110-1301 usa.
+#   with this program; if not, see <http://www.gnu.org/licenses/>.
 
 from smartcard.System import readers
 from smartcard.pcsc.PCSCPart10 import (SCARD_SHARE_DIRECT,
