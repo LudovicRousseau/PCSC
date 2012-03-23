de3ca4854319a4713a05b9a1846efe18f0574b8e
Ludovic Rousseau <ludovic.rousseau@free.fr>
2012-03-23 09:44:15 +0000

    Use getTlvProperties() and PCSCv2_PART10_PROPERTY_wIdVendor
    We now check the reader is a Gemalto one before sending an escape code
    
    
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@6244 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- UnitaryTests/control_get_firmware.py
+++ UnitaryTests/control_get_firmware.py
@@ -2,7 +2,7 @@
 
 """
 #   control_get_firmware.py: get firmware version of Gemalto readers
-#   Copyright (C) 2009-2010  Ludovic Rousseau
+#   Copyright (C) 2009-2012  Ludovic Rousseau
 """
 
 #   This program is free software; you can redistribute it and/or modify
@@ -20,16 +20,29 @@
 
 from smartcard.System import readers
 from smartcard.pcsc.PCSCPart10 import (SCARD_SHARE_DIRECT,
-    SCARD_LEAVE_CARD, SCARD_CTL_CODE)
+    SCARD_LEAVE_CARD, SCARD_CTL_CODE, getTlvProperties)
 
 for reader in readers():
     cardConnection = reader.createConnection()
     cardConnection.connect(mode=SCARD_SHARE_DIRECT,
         disposition=SCARD_LEAVE_CARD)
 
-    get_firmware = [0x02]
-    IOCTL_SMARTCARD_VENDOR_IFD_EXCHANGE = SCARD_CTL_CODE(1)
-    res = cardConnection.control(IOCTL_SMARTCARD_VENDOR_IFD_EXCHANGE,
-        get_firmware)
     print "Reader:", reader
-    print "Firmware:", "".join([chr(x) for x in res])
+
+    # properties returned by IOCTL_FEATURE_GET_TLV_PROPERTIES
+    properties = getTlvProperties(cardConnection)
+
+    # Gemalto devices supports a control code to get firmware
+    if properties['PCSCv2_PART10_PROPERTY_wIdVendor'] == 0x08E6:
+        get_firmware = [0x02]
+        IOCTL_SMARTCARD_VENDOR_IFD_EXCHANGE = SCARD_CTL_CODE(1)
+        res = cardConnection.control(IOCTL_SMARTCARD_VENDOR_IFD_EXCHANGE,
+            get_firmware)
+        print " Firmware:", "".join([chr(x) for x in res])
+    else:
+        print " Not a Gemalto reader"
+        try:
+            res = properties['PCSCv2_PART10_PROPERTY_sFirmwareID']
+            print " Firmware:", frimware
+        except KeyError:
+            print " PCSCv2_PART10_PROPERTY_sFirmwareID not supported"
