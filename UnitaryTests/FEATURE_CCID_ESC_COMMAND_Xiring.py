ad6c5c061517bd551a131ad8c7b1ad67c140e1c2
Ludovic Rousseau <ludovic.rousseau@free.fr>
2012-03-24 19:09:34 +0000

    Unitary test for FEATURE_CCID_ESC_COMMAND and Xiring Leo readers
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@6247 0ce88b0d-b2fd-0310-8134-9614164e65ea

---
+++ UnitaryTests/FEATURE_CCID_ESC_COMMAND_Xiring.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python
+
+"""
+#   FEATURE_CCID_ESC_COMMAND_Xiring.py: Unitary test for
+#   FEATURE_CCID_ESC_COMMAND
+#   Copyright (C) 2012  Ludovic Rousseau
+
+"""
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, see <http://www.gnu.org/licenses/>.
+
+from smartcard.System import readers
+from smartcard.pcsc.PCSCPart10 import (getFeatureRequest, hasFeature,
+    getTlvProperties, FEATURE_CCID_ESC_COMMAND, SCARD_SHARE_DIRECT)
+
+# use the first reader
+card_connection = readers()[0].createConnection()
+card_connection.connect(mode=SCARD_SHARE_DIRECT)
+
+# get CCID Escape control code
+feature_list = getFeatureRequest(card_connection)
+
+ccid_esc_command = hasFeature(feature_list, FEATURE_CCID_ESC_COMMAND)
+if ccid_esc_command is None:
+    raise Exception("The reader does not support FEATURE_CCID_ESC_COMMAND")
+
+# get the TLV PROPERTIES
+tlv = getTlvProperties(card_connection)
+
+# check we are using a Xiring Leo v1 or v2 reader
+if tlv['PCSCv2_PART10_PROPERTY_wIdVendor'] == 0x0F14 \
+    and (tlv['PCSCv2_PART10_PROPERTY_wIdProduct'] in [0x0037, 0x0038]):
+
+    # proprietary escape command for Xiring readers
+    version = [ord(c) for c in "VERSION"]
+    res = card_connection.control(ccid_esc_command, version)
+    print res
+    print ''.join([chr(x) for x in res])
+else:
+    print "Xiring Leo reader not found"
