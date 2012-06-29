500c0f4d7140cc11dc39002a0ee75486af62d5ad
Ludovic Rousseau <ludovic.rousseau@free.fr>
2012-06-29 12:11:07 +0000

    Fix comment
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@6379 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- src/readerfactory.h
+++ src/readerfactory.h
@@ -111,7 +111,7 @@
 		pthread_mutex_t powerState_lock;	/**< powerState mutex */
 
 		struct pubReaderStatesList *readerState; /**< link to the reader state */
-		/* we can't use READER_CONTEXT * here since eventhandler.h can't be
+		/* we can't use READER_STATE * here since eventhandler.h can't be
 		 * included because of circular dependencies */
 	};
 
