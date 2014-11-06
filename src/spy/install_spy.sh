a82a908265888fdaff098f63e74b9006c470bfb7
Ludovic Rousseau <ludovic.rousseau@free.fr>
2014-11-06 13:56:10 +0000

    Use ldconfig(8) to find the directory containing libpcsclite.so.1
    On a Debian system (using multiarch) the directory is no more /usr/lib
    but /usr/lib/x86_64-linux-gnu on a 64-bits Intel system.
    
    
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/PCSC@7027 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- src/spy/install_spy.sh
+++ src/spy/install_spy.sh
@@ -3,7 +3,19 @@
 # exit on the first error
 set -e
 
-cd /usr/lib
+# ldconfig --print-cache will return something like
+# libpcsclite.so.1 (libc6,x86-64) => /usr/lib/x86_64-linux-gnu/libpcsclite.so.1
+DIR=$(ldconfig --print-cache | grep libpcsclite.so.1)
+
+# get the right part only: /usr/lib/x86_64-linux-gnu/libpcsclite.so.1
+DIR=$(echo $DIR | cut -d'=' -f2 | cut -d' ' -f2)
+
+# get the directory part only: /usr/lib/x86_64-linux-gnu
+DIR=$(dirname $DIR)
+
+echo "Using directory:" $DIR
+
+cd $DIR
 
 NOSPY=libpcsclite_nospy.so.1
 
