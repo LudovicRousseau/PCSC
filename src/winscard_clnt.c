LudovicRousseau/PCSC

commit 9a21c82330fc4b73ac00faf2c5cf948da5f8ab07

SCardCancel(): fix a possible use after free
From Maksim Ivanov:
" It seems that there are still some issues with regard to the blocking
requests cancellation functionality in PC/SC-Lite:

2. Use-after-free possible in SCardCancel.

   This is probably a low-severity issue, as the deallocated memory will
   be accessed only for reading an int, which would be then used for
   deciding whether to fail fast or to send a request to the daemon
   (the latter is expected to fail anyway).

For #2, the suggestion is to move the reading of the
   currentContextMap->cancellable flag under the clientMutex lock.

   Also it may be advisable to change the SCardGetAndLockContext
   function interface so that it's more difficult to use it in thread-unsafe
   manner: remove the second parameter "int lock" and make the
   locking behavior the default one, and introduce another function
   that does no locks and returns a boolean instead of an
   SCONTEXTMAP* pointer. "

Thanks to Maksim Ivanov for the bug report and solution
"[Pcsclite-muscle] Data races related to SCardCancel"
http://lists.alioth.debian.org/pipermail/pcsclite-muscle/Week-of-Mon-20170116/000789.html

src/winscard_clnt.c
@@ -3072,6 +3072,7 @@ LONG SCardCancel(SCARDCONTEXT hContext)
 	LONG rv = SCARD_S_SUCCESS;
 	uint32_t dwClientID = 0;
 	struct cancel_struct scCancelStruct;
+	char cancellable;
 
 	PROFILE_START
 	API_TRACE_IN("%ld", hContext)
@@ -3079,14 +3080,17 @@ LONG SCardCancel(SCARDCONTEXT hContext)
 	/*
 	 * Make sure this context has been opened
 	 */
-	currentContextMap = SCardGetAndLockContext(hContext, FALSE);
+	currentContextMap = SCardGetAndLockContext(hContext, TRUE);
 	if (NULL == currentContextMap)
 	{
 		rv = SCARD_E_INVALID_HANDLE;
 		goto error;
 	}
 
-	if (! currentContextMap->cancellable)
+	cancellable = currentContextMap->cancellable;
+	(void)pthread_mutex_unlock(&currentContextMap->mMutex);
+
+	if (! cancellable)
 	{
 		rv = SCARD_S_SUCCESS;
 		goto error;

commit 7a3738d2f8e48c669b0e3f485cb9dcabeffc941a

fix SCardCancel() that was broken in version 1.8.21
Changes in 9a21c82330fc4b73ac00faf2c5cf948da5f8ab07 broke
SCardCancel().
The problem was that SCardCancel() was blocked until
SCardGetStatusChange() had finished. This is stupid because
SCardCancel() is supposed to cancel SCardGetStatusChange().

Thanks to Ran Benita for the bug report
"[Pcsclite-muscle] A recent change causes SCardCancel() to block?"
http://lists.alioth.debian.org/pipermail/pcsclite-muscle/2017-June/000921.html

src/winscard_clnt.c
@@ -3118,15 +3118,17 @@ LONG SCardCancel(SCARDCONTEXT hContext)
 	/*
 	 * Make sure this context has been opened
 	 */
-	currentContextMap = SCardGetAndLockContext(hContext);
+	(void)SCardLockThread();
+	currentContextMap = SCardGetContextTH(hContext);
+
 	if (NULL == currentContextMap)
 	{
+		(void)SCardUnlockThread();
 		rv = SCARD_E_INVALID_HANDLE;
 		goto error;
 	}
-
 	cancellable = currentContextMap->cancellable;
-	(void)pthread_mutex_unlock(&currentContextMap->mMutex);
+	(void)SCardUnlockThread();
 
 	if (! cancellable)
 	{