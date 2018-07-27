LudovicRousseau/PCSC

commit 984f84df10e2d0f432039e3b31f94c74e95092eb

Fix a rare race condition in SCardGetStatusChange()
Thanks to Maximilian Stein for the patch

[Pcsclite-muscle] Rare race condition in SCardGetStatusChange()
http://lists.infradead.org/pipermail/pcsclite-muscle/2018-April/001068.html

" Hello all,

recently we stumbled upon another (rare) race condition in the
SCardGetStatusChange() function. I especially ask Maksim Ivanov and
Florian Kaiser kindly to share their opinion about the problem and the
proposed fix, since they did a good job in improving
SCardGetStatusChange/SCardCancel in the recent past.

The problem:
The problem is basically the small gap between fetching the reader
states from pcscd [winscard_clnt.c:1741 and 2119] and registering for
event notifications [winscard_clnt.c:2070]. If a notification is sent in
this time period, SCardGetStatusChange() will sleep until another event
or the internal time-out is reached. Consider the following flow of
execution and events:

Precondition: [client] knows the current state of all readers from a
previous call to SCardGetStatusChange() and no changes happened since
then. Basically consider a thread that monitors the reader states which
calls SCardGetStatusChange() in a loop.
1. [client] calls SCardGetStatusChange() and fetches the reader states
from pcscd (line 1741). The fetched states are equal to the already
known states.
2. [pcscd] notices a change in any terminal state (smartcard movement or
number of connected clients to the smartcard changes) and sends a signal
to all *registered* clients via EHSignalEventToClients(). [client] is
not yet registered for event notifications, so EHSignalEventToClients()
won't send anything.
3. [client] continues execution and compares the given reader states to
the states fetched before [pcscd] noticed the change. The states are
equal so it registers for event notifications (line 2070). No
notification was sent, so [client] sleeps until the internal time-out
(60 seconds) is reached.

src/winscard_clnt.c
@@ -388,6 +388,8 @@ static LONG SCardGetSetAttrib(SCARDHANDLE hCard, int command, DWORD dwAttrId,
 	LPBYTE pbAttr, LPDWORD pcbAttrLen);
 
 static LONG getReaderStates(SCONTEXTMAP * currentContextMap);
+static LONG getReaderStatesAndRegisterForEvents(SCONTEXTMAP * currentContextMap);
+static LONG unregisterFromEvents(SCONTEXTMAP * currentContextMap);
 
 /*
  * Thread safety functions
@@ -1736,7 +1738,7 @@ LONG SCardGetStatusChange(SCARDCONTEXT hContext, DWORD dwTimeout,
 	}
 
 	/* synchronize reader states with daemon */
-	rv = getReaderStates(currentContextMap);
+	rv = getReaderStatesAndRegisterForEvents(currentContextMap);
 	if (rv != SCARD_S_SUCCESS)
 		goto end;
 
@@ -2056,23 +2058,15 @@ LONG SCardGetStatusChange(SCARDCONTEXT hContext, DWORD dwTimeout,
 
 			/* Only sleep once for each cycle of reader checks. */
 			{
-				struct wait_reader_state_change waitStatusStruct;
+				struct wait_reader_state_change waitStatusStruct = {0};
 				struct timeval before, after;
 
 				gettimeofday(&before, NULL);
 
-				waitStatusStruct.timeOut = dwTime;
 				waitStatusStruct.rv = SCARD_S_SUCCESS;
 
 				/* another thread can do SCardCancel() */
 				currentContextMap->cancellable = TRUE;
-
-				rv = MessageSendWithHeader(CMD_WAIT_READER_STATE_CHANGE,
-					currentContextMap->dwClientID,
-					sizeof(waitStatusStruct), &waitStatusStruct);
-
-				if (rv != SCARD_S_SUCCESS)
-					goto end;
 
 				/*
 				 * Read a message from the server
@@ -2089,20 +2083,7 @@ LONG SCardGetStatusChange(SCARDCONTEXT hContext, DWORD dwTimeout,
 				if (SCARD_E_TIMEOUT == rv)
 				{
 					/* ask server to remove us from the event list */
-					rv = MessageSendWithHeader(CMD_STOP_WAITING_READER_STATE_CHANGE,
-						currentContextMap->dwClientID,
-						sizeof(waitStatusStruct), &waitStatusStruct);
-
-					if (rv != SCARD_S_SUCCESS)
-						goto end;
-
-					/* Read a message from the server */
-					rv = MessageReceive(&waitStatusStruct,
-						sizeof(waitStatusStruct),
-						currentContextMap->dwClientID);
-
-					if (rv != SCARD_S_SUCCESS)
-						goto end;
+					rv = unregisterFromEvents(currentContextMap);
 				}
 
 				if (rv != SCARD_S_SUCCESS)
@@ -2116,7 +2097,7 @@ LONG SCardGetStatusChange(SCARDCONTEXT hContext, DWORD dwTimeout,
 				}
 
 				/* synchronize reader states with daemon */
-				rv = getReaderStates(currentContextMap);
+				rv = getReaderStatesAndRegisterForEvents(currentContextMap);
 				if (rv != SCARD_S_SUCCESS)
 					goto end;
 
@@ -2147,6 +2128,8 @@ LONG SCardGetStatusChange(SCARDCONTEXT hContext, DWORD dwTimeout,
 
 end:
 	Log1(PCSC_LOG_DEBUG, "Event Loop End");
+
+	(void)unregisterFromEvents(currentContextMap);
 
 	(void)pthread_mutex_unlock(&currentContextMap->mMutex);
 
@@ -3547,5 +3530,51 @@ static LONG getReaderStates(SCONTEXTMAP * currentContextMap)
 		return rv;
 
 	return SCARD_S_SUCCESS;
+}
+
+static LONG getReaderStatesAndRegisterForEvents(SCONTEXTMAP * currentContextMap)
+{
+	int32_t dwClientID = currentContextMap->dwClientID;
+	LONG rv;
+
+	/* Get current reader states from server and register on event list */
+	rv = MessageSendWithHeader(CMD_WAIT_READER_STATE_CHANGE, dwClientID,
+		0, NULL);
+	if (rv != SCARD_S_SUCCESS)
+		return rv;
+
+	/* Read a message from the server */
+	rv = MessageReceive(&readerStates, sizeof(readerStates), dwClientID);
+	return rv;
+}
+
+static LONG unregisterFromEvents(SCONTEXTMAP * currentContextMap)
+{
+	int32_t dwClientID = currentContextMap->dwClientID;
+	LONG rv;
+	struct wait_reader_state_change waitStatusStruct = {0};
+
+	/* ask server to remove us from the event list */
+	rv = MessageSendWithHeader(CMD_STOP_WAITING_READER_STATE_CHANGE,
+		dwClientID, 0, NULL);
+	if (rv != SCARD_S_SUCCESS)
+		return rv;
+
+	/* This message can be the response to
+	 * CMD_STOP_WAITING_READER_STATE_CHANGE, an event notification or a
+	 * cancel notification.
+	 * The server side ensures, that no more messages will be sent to
+	 * the client. */
+
+	rv = MessageReceive(&waitStatusStruct, sizeof(waitStatusStruct),
+		dwClientID);
+	if (rv != SCARD_S_SUCCESS)
+		return rv;
+
+	/* if we received a cancel event the return value will be set
+	 * accordingly */
+	rv = waitStatusStruct.rv;
+
+	return rv;
 }
 

src/eventhandler.c
@@ -69,6 +69,8 @@ LONG EHRegisterClientForEvent(int32_t filedes)
 
 	(void)list_append(&ClientsWaitingForEvent, &filedes);
 
+	(void)MSGSendReaderStates(filedes);
+
 	(void)pthread_mutex_unlock(&ClientsWaitingForEvent_lock);
 
 	return SCARD_S_SUCCESS;

src/winscard_msg.h
@@ -46,7 +46,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 /** Major version of the current message protocol */
 #define PROTOCOL_VERSION_MAJOR 4
 /** Minor version of the current message protocol */
-#define PROTOCOL_VERSION_MINOR 3
+#define PROTOCOL_VERSION_MINOR 4
 
 	/**
 	 * @brief Information transmitted in \ref CMD_VERSION Messages.

src/winscard_svc.c
@@ -411,24 +411,21 @@ static void ContextThread(LPVOID newContext)
 
 			case CMD_WAIT_READER_STATE_CHANGE:
 			{
-				struct wait_reader_state_change waStr;
+				/* nothing to read */
 
-				READ_BODY(waStr);
+#ifdef USE_USB
+				/* wait until all readers are ready */
+				RFWaitForReaderInit();
+#endif
 
-				/* add the client fd to the list */
+				/* add the client fd to the list and dump the readers state */
 				EHRegisterClientForEvent(filedes);
-
-				/* We do not send anything here.
-				 * Either the client will timeout or the server will
-				 * answer if an event occurs */
 			}
 			break;
 
 			case CMD_STOP_WAITING_READER_STATE_CHANGE:
 			{
-				struct wait_reader_state_change waStr;
-
-				READ_BODY(waStr);
+				struct wait_reader_state_change waStr = {0};
 
 				/* remove the client fd from the list */
 				waStr.rv = EHUnregisterClientForEvent(filedes);
@@ -810,7 +807,7 @@ exit:
 LONG MSGSignalClient(uint32_t filedes, LONG rv)
 {
 	uint32_t ret;
-	struct wait_reader_state_change waStr;
+	struct wait_reader_state_change waStr = {0};
 
 	Log2(PCSC_LOG_DEBUG, "Signal client: %d", filedes);
 
@@ -819,6 +816,18 @@ LONG MSGSignalClient(uint32_t filedes, LONG rv)
 
 	return ret;
 } /* MSGSignalClient */
+
+LONG MSGSendReaderStates(uint32_t filedes)
+{
+	uint32_t ret;
+
+	Log2(PCSC_LOG_DEBUG, "Send reader states: %d", filedes);
+
+	/* dump the readers state */
+	ret = MessageSend(readerStates, sizeof(readerStates), filedes);
+
+	return ret;
+}
 
 static LONG MSGAddContext(SCARDCONTEXT hContext, SCONTEXT * threadContext)
 {

src/winscard_svc.h
@@ -45,5 +45,6 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 	void ContextsDeinitialize(void);
 	LONG CreateContextThread(uint32_t *);
 	LONG MSGSignalClient(uint32_t filedes, LONG rv);
+	LONG MSGSendReaderStates(uint32_t filedes);
 
 #endif

commit 54224a4cbf60f95f2da5f0978c62656f3369b700

Fix SCardGetStatusChange() broken in a previous patch 984f84df
The patch in 984f84df10e2d0f432039e3b31f94c74e95092eb broke the
execution of SCardGetStatusChange() when SCardCancel() is used.

The client was waiting for a message that was never sent by the server.

Fortunately the broken code was never released in a stable version of
pcsc-lite.

Thanks to Sam Van Den Berge for the bug report
"[Pcsclite-muscle] SCardCancel broken"
http://lists.infradead.org/pipermail/pcsclite-muscle/2018-July/001096.html

Hello,

It seems like SCardGetStatusChange is currently broken.
This can be tested with UnitaryTests/SCardCancel.

Before commit 984f84df10e2d0f432039e3b31f94c74e95092eb:

$ LD_LIBRARY_PATH=../src/.libs/ ./SCardCancel
SCardEstablishContext:[0x00000000] Command successful.
Press Enter to cancel within 3 seconds
Entering blocking call

Calling SCardCancel...
SCardGetStatusChange:[0x80100002] Command cancelled.
Blocking call canceled
Good
SCardCancel:[0x00000000] Command successful.
SCardReleaseContext:[0x00000000] Command successful.
Waiting thread...

After commit 984f84df10e2d0f432039e3b31f94c74e95092eb:

$ LD_LIBRARY_PATH=../src/.libs/ ./SCardCancel
SCardEstablishContext:[0x00000000] Command successful.
Press Enter to cancel within 3 seconds
Entering blocking call

Calling SCardCancel...
SCardCancel:[0x00000000] Command successful.

<<<<<<<<< SCardGetStatusChange hangs here >>>>>>>>>

I don't have a fix but I just wanted to report this. Besides this big
thank you for all your great work!

Kr,
Sam.

src/winscard_clnt.c
@@ -2129,7 +2129,10 @@ LONG SCardGetStatusChange(SCARDCONTEXT hContext, DWORD dwTimeout,
 end:
 	Log1(PCSC_LOG_DEBUG, "Event Loop End");
 
-	(void)unregisterFromEvents(currentContextMap);
+	/* if SCardCancel() has been used then the client is already
+	 * unregistered */
+	if (SCARD_E_CANCELLED != rv)
+		(void)unregisterFromEvents(currentContextMap);
 
 	(void)pthread_mutex_unlock(&currentContextMap->mMutex);
 