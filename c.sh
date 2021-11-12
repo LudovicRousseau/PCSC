d81489a7b2f34d368e99b687466b01ad5939b50a
Ludovic Rousseau <ludovic.rousseau@free.fr>
2021-11-12 17:04:40 +0100

    c.sh: /lib is now a symbolic link to /usr/lib on GNU/Linux
    See https://www.freedesktop.org/wiki/Software/systemd/TheCaseForTheUsrMerge/

--- c.sh
+++ c.sh
@@ -7,7 +7,7 @@ set -x
         --sysconfdir=/etc \
         --enable-maintainer-mode \
         --enable-twinserial \
-        --with-systemdsystemunitdir=/lib/systemd/system \
+        --with-systemdsystemunitdir=/usr/lib/systemd/system \
         --enable-usbdropdir=/usr/lib/pcsc/drivers \
         --enable-ipcdir=/run/pcscd \
         --libdir=/usr/lib/x86_64-linux-gnu \
