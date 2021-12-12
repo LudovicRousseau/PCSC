4a2fb8f770ec307c6bca50b8176674dedab2a205
Ludovic Rousseau <ludovic.rousseau@free.fr>
2021-12-12 15:44:24 +0100

    readerfactory.h: fix data race with contexts field
    The .contexts field is used in read/write from different threads. We
    must protect the accesses by declaring it _Atomic (from C11).
    
    Thanks to andrei-datcu for the patch
    "No data races in EHStatusHandlerThread #112"
    https://github.com/LudovicRousseau/PCSC/pull/112

--- src/readerfactory.h
+++ src/readerfactory.h
@@ -125,7 +125,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 		int slot;				/**< Current Reader Slot */
 		_Atomic SCARDHANDLE hLockId;	/**< Lock Id */
 		int LockCount;			/**< number of recursive locks */
-		int32_t contexts;		/**< Number of open contexts */
+		_Atomic int32_t contexts;		/**< Number of open contexts */
 		int * pFeeds;			/**< Number of shared client to lib */
 		int * pMutex;			/**< Number of client to mutex */
 		int powerState;			/**< auto power off state */
