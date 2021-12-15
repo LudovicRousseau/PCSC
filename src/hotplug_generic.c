25db7bc5504cb74336d45224ed33bd4ea88e835f
Ludovic Rousseau <ludovic.rousseau@free.fr>
2021-12-15 11:48:52 +0100

    hotplug_generic: Fix compiler warnings:
    hotplug_generic.c:53:6: warning: no previous prototype for ‘HPSearchHotPluggables’ [-Wmissing-prototypes]
       53 | LONG HPSearchHotPluggables(void)
          |      ^~~~~~~~~~~~~~~~~~~~~
    hotplug_generic.c:58:7: warning: no previous prototype for ‘HPRegisterForHotplugEvents’ [-Wmissing-prototypes]
       58 | ULONG HPRegisterForHotplugEvents(void)
          |       ^~~~~~~~~~~~~~~~~~~~~~~~~~
    hotplug_generic.c:63:6: warning: no previous prototype for ‘HPStopHotPluggables’ [-Wmissing-prototypes]
       63 | LONG HPStopHotPluggables(void)
          |      ^~~~~~~~~~~~~~~~~~~
    hotplug_generic.c:68:6: warning: no previous prototype for ‘HPReCheckSerialReaders’ [-Wmissing-prototypes]
       68 | void HPReCheckSerialReaders(void)
          |      ^~~~~~~~~~~~~~~~~~~~~~

--- src/hotplug_generic.c
+++ src/hotplug_generic.c
@@ -43,6 +43,8 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "config.h"
 #include "pcsclite.h"
 
+#include "hotplug.h"
+
 #ifndef TRUE
 #define TRUE 1
 #define FALSE 0
