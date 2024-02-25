1cc192d81f24c5b1ade2cf7cd8cffb4a12fced33
Ludovic Rousseau <ludovic.rousseau@free.fr>
2024-02-25 18:56:12 +0100

    Create a setup_spy.sh script
    The script set and display the correct value to use for
    LIBPCSCLITE_DELEGATE.

--- src/spy/setup_spy.sh
+++ src/spy/setup_spy.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 
-# exit on the first error
-set -e
-
 # ldconfig --print-cache will return something like
 # libpcsclite.so.1 (libc6,x86-64) => /usr/lib/x86_64-linux-gnu/libpcsclite.so.1
 DIR=$(ldconfig --print-cache | grep libpcsclite.so.1)
 
 # get the right part only: /usr/lib/x86_64-linux-gnu/libpcsclite.so.1
-DIR=$(echo $DIR | cut -d'=' -f2 | cut -d' ' -f2)
+DIR=$(echo "$DIR" | cut -d'=' -f2 | cut -d' ' -f2)
 
 # get the directory part only: /usr/lib/x86_64-linux-gnu
-DIR=$(dirname $DIR)
+DIR=$(dirname "$DIR")
 
-echo "Using directory:" $DIR
-
-cd $DIR
-
-# Use the real library again
-mv libpcsclite_nospy.so.1 libpcsclite.so.1.0.0
+cmd="export LIBPCSCLITE_DELEGATE=$DIR/libpcscspy.so"
+echo "$cmd"
+$cmd
