586b77878b6d37248523e75b4f327f14e4467ff4
Ludovic Rousseau <ludovic.rousseau@free.fr>
2024-03-20 11:37:14 +0100

    Fix compilation error in mode --enable-strict
    Log2() is not available in the redirection lib.
    
    /usr/bin/ld: ./.libs/libpcsclite.so: undefined reference to `log_msg'
    
    Log2() is used in SYS_RandomInt() that is not called by the redirection
    library so no problem here.

--- src/sys_unix.c
+++ src/sys_unix.c
@@ -116,7 +116,9 @@ INTERNAL int SYS_RandomInt(void)
 	ret = getrandom(c, sizeof c, 0);
 	if (-1 == ret)
 	{
+#ifndef LIBREDIRECT
 		Log2(PCSC_LOG_ERROR, "getrandom() failed: %s", strerror(errno));
+#endif
 		return lrand48();
 	}
 	// this loop avoids trap representations that may occur in the naive solution
