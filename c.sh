763e4fe45235e72bcf71e492042dc1d61c0263db
Ludovic Rousseau <ludovic.rousseau@free.fr>
2024-05-19 22:20:44 +0200

    c.sh: Do not define TMPDIR as it confuses clang
    meson.build:12:0: ERROR: Compiler cc cannot compile programs.
    
    Use TMP_DIR instead.

--- c.sh
+++ c.sh
@@ -3,19 +3,19 @@
 set -x
 set -e
 
-BUILDDIR=builddir
-TMPDIR=/tmp/pcsc
+BUILD_DIR=builddir
+TMP_DIR=/tmp/pcsc
 
-rm -rf "$BUILDDIR"
+rm -rf "$BUILD_DIR"
 
-meson setup "$BUILDDIR" \
+meson setup "$BUILD_DIR" \
 	--prefix /usr \
 	-Dsystemdunit=system \
 	"$@"
 
-cd "$BUILDDIR"
+cd "$BUILD_DIR"
 meson compile
 
-rm -rf "$TMPDIR"
-DESTDIR="$TMPDIR" meson install
-find "$TMPDIR"
+rm -rf "$TMP_DIR"
+DESTDIR="$TMP_DIR" meson install
+find "$TMP_DIR"
